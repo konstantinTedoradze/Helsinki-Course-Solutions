977dc26205ac5cf49d7d8d38029489fc19bd861d
konstantinTedoradze <97909996+konstantinTedoradze@users.noreply.github.com>
2022-03-14 22:13:27 +0200

    update part1 contents

---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,55 @@
+import React,{ useState } from 'react';
+
+function App() {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ];
+
+
+
+  const points = { 0: 1, 1: 3, 2: 4, 3: 2, 4: 1, 5: 2, 6: 4};
+
+  const [selected, setSelected] = useState(Math.floor(Math.random() * (anecdotes.length)));
+  const [copy,setCopy] = useState({...points});
+
+  let max = Object.keys(copy).reduce((a, b) => copy[a] > copy[b] ? a : b);
+
+  const [maxVotes,setMaxVotes] = useState(max);
+
+  function getRandomArbitrary() {
+    console.log(Math.floor(Math.random() * (anecdotes.length)),'random number');
+    let ramdomNumbers = Math.floor(Math.random() * (anecdotes.length)); 
+    setSelected(ramdomNumbers);
+  }
+
+  
+  const onVoteClicked = () => {
+    setMaxVotes(max);
+    copy[selected] += 1;
+    setCopy({...copy});
+    console.log(copy,'in function');
+  }
+
+  console.log('koteeee');
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {copy[selected]} votes</p>
+      <button onClick={onVoteClicked}>Vote</button>
+      <button onClick={getRandomArbitrary}>next anecdote</button>
+      <h3>Anecdote with most votes</h3>
+      <p>{anecdotes[maxVotes]}</p>
+      <p>has {copy[maxVotes]}</p>
+    </div>
+  );
+}
+
+export default App;
